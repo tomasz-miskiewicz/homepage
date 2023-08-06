abbadd58db4b7b24298f5254def03e17513e7a65
Tomek Miśkiewicz <t.z.miskiewicz@gmail.com>
2023-08-07 00:08:52 +0200

    first custom function with 3 params

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 console.log('Hej! to jest tekst ze skryptu js w main.js');
-
+/*
 const myName = 'Tomek';
 const mySurname = 'Miśkiewicz';
 let favouriteColour = 'niebieski';
+*/
+// console.log (`Hej! Tu ${myName} ${mySurname}, mój ulubiony kolor dziś to: ${favouriteColour}`);
 
-console.log (`Hej! Tu ${myName} ${mySurname}, mój ulubiony kolor dziś to: ${favouriteColour}`);
+function welcomeReader(myName, mySurname, favouriteColour) {
+    return console.log(`Hej! Tu ${myName} ${mySurname}, mój ulubiony kolor to: ${favouriteColour}`)
+}
+
+welcomeReader("Jan", "Kowalski", "czerwony");
+welcomeReader("Bolek", "Nowakowski", "czarny");
